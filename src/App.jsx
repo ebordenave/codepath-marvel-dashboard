f5d07c59579f7ad65926d8cb6252abdc4a2d114b
ebordenave <edward.bordenave@gmail.com>
2024-03-24 20:28:59 -0400

    update styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // import { useState } from "react";
 import "./App.css";
+import { SearchBar } from "./components/SearchBar/SearchBar.jsx";
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -7,9 +8,12 @@ function App() {
   return (
     <>
       <div className="wrapper">
-        <div className="box a">A</div>
+        <div className="box a"></div>
         <div className="box b">B</div>
-        <div className="box c">C</div>
+        <div className="box c">
+          <SearchBar />
+        </div>
+
         <div className="box d">D</div>
         <div className="box e">E</div>
       </div>
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,9 @@
+import "./SearchBar.css";
+
+export const SearchBar = () => {
+  return (
+    <>
+      <input type="text" />
+    </>
+  );
+};
